c9cd012a5f92a70abfca0d61acf643f0c5888e3b
TommySma <tsmaevicb8@gmail.com>
2026-06-12 13:42:51 -0300

    empiezo el commentPost

---
+++ src/screens/CommentPost/CommentPost.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text, TextInput, FlatList, Pressable, StyleSheet, ActivityIndicator} from 'react-native';
+import {db, auth} from '../../firebase/config';
+
+function CommentPost(props){
+    
+
+}
+export default CommentPost;
